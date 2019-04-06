74e917d0fee30351c6b61c651c8621d57ea0e30b
Andrew Mittleider <amittleider@users.noreply.github.com>
2019-04-06 23:36:52 +0200

    Deserialize IEXSymbols calls

---
+++ IEXDotNet/IEXFormatter.cs
@@ -0,0 +1,18 @@
+﻿using IEXDotNet.IEXDataStructures;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IEXDotNet
+{
+    public class IEXFormatter
+    {
+        public List<IEXSymbol> FormatIEXSymbols(string iexSymbols)
+        {
+            List<IEXSymbol> symbolsList = JsonConvert.DeserializeObject<List<IEXSymbol>>(iexSymbols);
+
+            return symbolsList;
+        }
+    }
+}
