4c57acb81cb8a1d3a1a1a4f68df8d1d2c8fed0ca
Andrew Mittleider <1625951+amittleider@users.noreply.github.com>
2020-04-10 10:53:36 +0200

    Implement IEX Last (#14)

---
+++ IEXDotNet/IEXDataStructures/IexTopsLast.cs
@@ -0,0 +1,10 @@
+﻿namespace IEXDotNet.IEXDataStructures
+{
+    public class IexTopsLast
+    {
+        public string Symbol;
+        public double Price;
+        public int Size;
+        public long Time;
+    }
+}
